6ec1fca0dbaf21c2a924cfe90c0311a8219541f7
bearkent <39718144+bearkent@users.noreply.github.com>
2024-02-10 11:58:42 -0700

    change pin numbers

--- motorruntest2.py
+++ motorruntest2.py
@@ -6,26 +6,26 @@ import time
 import RPi.GPIO as GPIO
 
 # Next we setup the pins for use!
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
-GPIO.setup(12,GPIO.OUT)
-GPIO.setup(13,GPIO.OUT)
+GPIO.setup(32,GPIO.OUT)
+GPIO.setup(33,GPIO.OUT)
 
 print('Starting motor sequence!')
 
 
     # Makes the motor spin one way for 3 seconds
 print("test forward")
-GPIO.output(12, True)
-GPIO.output(13, False)
+GPIO.output(32, True)
+GPIO.output(33, False)
 time.sleep(3)
     # Spins the other way for a further 3 seconds
 print("test backward")
-GPIO.output(12, False)
-GPIO.output(13, True)
+GPIO.output(32, False)
+GPIO.output(33, True)
 time.sleep(3)
     # If a keyboard interrupt is detected then it exits cleanly!
 print('Finishing up!')
-GPIO.output(12, False)
-GPIO.output(13, False)
+GPIO.output(32, False)
+GPIO.output(33, False)
 quit()
