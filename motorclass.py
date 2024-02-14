d902d84cba462da47c7eed0e31aba92e11fea422
bearkent <39718144+bearkent@users.noreply.github.com>
2024-02-14 10:22:47 -0700

    trying to make class work

--- motorclass.py
+++ motorclass.py
@@ -7,14 +7,14 @@ class Motor:
 
         self.pinlist = pinlist
 
-        pinlist[0] = m1pin1  #motor1 pin1
-        pinlist[1] = m1pin2
-        pinlist[2] = m2pin1
-        pinlist[3] = m2pin2
-        pinlist[4] = m3pin1
-        pinlist[5] = m3pin2
-        pinlist[6] = m4pin1
-        pinlist[7] = m4pin2
+        # pinlist[0] = self.pinlist[0]  #motor1 pin1
+        # pinlist[1] = self.pinlist[1]
+        # pinlist[2] = self.pinlist[2]
+        # pinlist[3] = self.pinlist[3]
+        # pinlist[4] = self.pinlist[4]
+        # pinlist[5] = self.pinlist[5]
+        # pinlist[6] = self.pinlist[6]
+        # pinlist[7] = self.pinlist[7]
 
         
 
@@ -22,61 +22,61 @@ class Motor:
     # Next we setup the pins for use!
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
-        GPIO.setup(m1pin1,GPIO.OUT)
-        GPIO.setup(m1pin2,GPIO.OUT)
-        GPIO.setup(m2pin1,GPIO.OUT)
-        GPIO.setup(m2pin2,GPIO.OUT)
-        GPIO.setup(m3pin1,GPIO.OUT)
-        GPIO.setup(m3pin2,GPIO.OUT)
-        GPIO.setup(m4pin1,GPIO.OUT)
-        GPIO.setup(m4pin2,GPIO.OUT)
+        GPIO.setup(self.pinlist[0],GPIO.OUT)
+        GPIO.setup(self.pinlist[1],GPIO.OUT)
+        GPIO.setup(self.pinlist[2],GPIO.OUT)
+        GPIO.setup(self.pinlist[3],GPIO.OUT)
+        GPIO.setup(self.pinlist[4],GPIO.OUT)
+        GPIO.setup(self.pinlist[5],GPIO.OUT)
+        GPIO.setup(self.pinlist[6],GPIO.OUT)
+        GPIO.setup(self.pinlist[7],GPIO.OUT)
 
     def motor1(self, k:bool, t:int):
         if k:
             print(f"motor 1 forward for {t} milliseconds")
-            GPIO.output(m1pin1, True)
-            GPIO.output(m1pin2, False)
+            GPIO.output(self.pinlist[0], True)
+            GPIO.output(self.pinlist[1], False)
             time.sleep(t/1000)
         else:
             print(f"motor 1 backward for {t} milliseconds")
-            GPIO.output(m1pin1, False)
-            GPIO.output(m1pin2, True)
+            GPIO.output(self.pinlist[0], False)
+            GPIO.output(self.pinlist[1], True)
             time.sleep(t/1000)
 
     def motor2(self, k:bool, t:int):
         if k:
             print(f"motor 2 forward for {t} milliseconds")
-            GPIO.output(m2pin1, True)
-            GPIO.output(m2pin2, False)
+            GPIO.output(self.pinlist[2], True)
+            GPIO.output(self.pinlist[3], False)
             time.sleep(t/1000)
         else:
             print(f"motor 2 backward for {t} milliseconds")
-            GPIO.output(m2pin1, False)
-            GPIO.output(m2pin2, True)
+            GPIO.output(self.pinlist[2], False)
+            GPIO.output(self.pinlist[3], True)
             time.sleep(t/1000)
 
     def motor3(self, k:bool, t:int):
         if k:
             print(f"motor 3 forward for {t} milliseconds")
-            GPIO.output(m3pin1, True)
-            GPIO.output(m3pin2, False)
+            GPIO.output(self.pinlist[4], True)
+            GPIO.output(self.pinlist[5], False)
             time.sleep(t/1000)
         else:
             print(f"motor 3 backward for {t} milliseconds")
-            GPIO.output(m3pin1, False)
-            GPIO.output(m3pin2, True)
+            GPIO.output(self.pinlist[4], False)
+            GPIO.output(self.pinlist[5], True)
             time.sleep(t/1000)
 
     def motor4(self, k:bool, t:int):
         if k:
             print(f"motor 4 forward for {t} milliseconds")
-            GPIO.output(m4pin1, True)
-            GPIO.output(m4pin2, False)
+            GPIO.output(self.pinlist[6], True)
+            GPIO.output(self.pinlist[7], False)
             time.sleep(t/1000)
         else:
             print(f"motor 4 backward for {t} milliseconds")
-            GPIO.output(m4pin1, False)
-            GPIO.output(m4pin2, True)
+            GPIO.output(self.pinlist[6], False)
+            GPIO.output(self.pinlist[7], True)
             time.sleep(t/1000)
 
 
