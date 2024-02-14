55fde6117235d99d95936c603b57910ff4ca7c5d
bearkent <39718144+bearkent@users.noreply.github.com>
2024-02-14 09:10:09 -0700

    made new file to test the oop

---
+++ motorrunttest3.py
@@ -0,0 +1,11 @@
+from motor import Motor
+
+pinlist = [12, 13, 1, 5, 17, 18, 22, 23]
+
+motor = Motor(pinlist)
+
+motor.setuppins()
+motor.motor1(True,1000)
+motor.motor2(True,1000)
+motor.motor3(True,1000)
+motor.motor4(True,1000)
