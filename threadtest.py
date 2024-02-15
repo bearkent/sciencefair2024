658736b42b2cc1e2be9f8ea51d5c52b8b95cb86c
bearkent <39718144+bearkent@users.noreply.github.com>
2024-02-14 19:21:28 -0700

    cleaning code

--- threadtest.py
+++ threadtest.py
@@ -6,10 +6,10 @@ pinlist = [12, 13, 1, 5, 17, 18, 22, 23]
 m = Motor(pinlist)
 
 m.setuppins()
-t1 = t.Thread(m.motor1, [True, 1000])
-t2 = t.Thread(m.motor2, [True, 1000])
-t3 = t.Thread(m.motor3, [True, 1000])
-t4 = t.Thread(m.motor4, [True, 1000])
+t1 = t.Thread(target=m.motor1, args=[True, 1000])
+t2 = t.Thread(target=m.motor2, args=[True, 1000])
+t3 = t.Thread(target=m.motor3, args=[True, 1000])
+t4 = t.Thread(target=m.motor4, args=[True, 1000])
 
 t1.start()
 t2.start()
