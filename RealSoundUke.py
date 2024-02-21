c0b2e3356eea43a26924a15253ef306384c519bb
Bear Kent <davidbearkent@gmail.com>
2024-02-21 13:04:08 -0700

    not sensitive enough

--- RealSoundUke.py
+++ RealSoundUke.py
@@ -88,7 +88,7 @@ class FFT:
         self.i=0
         
         for self.y in self.ys:
-            if self.y>=0.5:
+            if self.y>=0.3:
                 self.indecies.append(self.i/2)
                 print(f"index: {self.i}")
                 self.i+=1
