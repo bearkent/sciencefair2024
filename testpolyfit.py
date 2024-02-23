f39e18d556a7ba78e915651ea79e86f355e2f546
Bear Kent <davidbearkent@gmail.com>
2024-02-22 17:18:50 -0700

    small fix

--- testpolyfit.py
+++ testpolyfit.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 xs = np.linspace(0,1,10)
-ys=np.array(0,1,2,3,4,5,4,3,2,1)
+ys=np.array([0,1,2,3,4,5,4,3,2,1])
 
 p = np.polyfit(xs,ys,4)
 t=np.linspace(0,1,50)
