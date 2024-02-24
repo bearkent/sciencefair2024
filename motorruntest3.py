13fb2a306490c86e3da3edac8f8875e96b672352
Bear Kent <davidbearkent@gmail.com>
2024-02-23 20:43:12 -0700

    making sure to not break strings

--- motorruntest3.py
+++ motorruntest3.py
@@ -5,7 +5,7 @@ pinlist = [12, 13, 1, 5, 17, 18, 22, 23]
 motor = Motor(pinlist)
 
 motor.setuppins()
-motor.motor1(True,1000)
-motor.motor2(True,1000)
-motor.motor3(True,1000)
-motor.motor4(True,1000)
+motor.motor1(True,500)
+motor.motor2(True,500)
+motor.motor3(True,500)
+motor.motor4(True,500)
