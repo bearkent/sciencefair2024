6dcc5beaa5711b2357584bee5d296aeca474e033
Bear Kent <davidbearkent@gmail.com>
2024-02-23 22:19:57 -0700

    small mean change

--- RealSoundUke.py
+++ RealSoundUke.py
@@ -168,10 +168,10 @@ class PowerSpectrum:
         print(self.string3)
         print(self.string4)
 
-        self.string1freq=mean(self.string1)
-        self.string2freq=mean(self.string2)
-        self.string3freq=mean(self.string3)
-        self.string4freq=mean(self.string4)
+        if len(self.string1freq!=1): self.string1freq=mean(self.string1)
+        if len(self.string2freq!=1): self.string2freq=mean(self.string2)
+        if len(self.string3freq!=1): self.string3freq=mean(self.string3)
+        if len(self.string4freq!=1): self.string4freq=mean(self.string4)
 
         print(self.string1freq)
         print(self.string2freq)
